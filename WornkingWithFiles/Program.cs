42e1d7593fa906135f57c7e15959e865dcc6c97c
misho <mishojaparidze@gmail.com>
2023-09-10 17:58:17 +0400

    changes

---
+++ WornkingWithFiles/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace WornkingWithFiles
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var inputFilePath = @"file.txt";
+
+            string sr = File.OpenText(inputFilePath).ReadToEnd();
+
+            Console.WriteLine(sr);
+
+
+            string sr2 = new StreamReader(inputFilePath).ReadToEnd();
+            
+
+
+
+
+        }
+
+    }
+}
